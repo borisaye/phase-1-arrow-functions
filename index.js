b92c7aadc40b92b0d93117e5307c9a7f8f686ce0
Borisaye <boris_aroche@hotmail.com>
2023-04-06 20:43:38 -0400

    .

---
+++ index.js
@@ -0,0 +1,17 @@
+function divide(num1, num2){
+
+divide = num1/num2;
+return divide;
+
+}
+
+const square = num1 => {
+    let powerOf = num1 ** num1;
+    return powerOf;
+}
+
+const add = (num1, num2) => {
+    let additionProb = num1 + num2;
+    return additionProb;
+
+}
